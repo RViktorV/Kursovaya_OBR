1ae75b453a4ac3f0f551623e21226a8ee7b1c037
Viktor Rzhavykh <rgavih@yandex.ru>
2024-04-02 07:26:23 +0700

    add def loading_json(file_json)

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,11 @@
-def filter_operations():
-    pass
+import json
+def loading_json(file_json):
+    """
+    Функция загружает данные из файла operations.json
+    :param file_json:
+    :return:
+    """
+    with (open(file_json, 'r') as file:
+        return  json.load(file)
+
+
