c5eb1ad03e8286e2fde0e633089111eabe85459f
Viktor Rzhavykh <rgavih@yandex.ru>
2024-04-02 12:12:43 +0700

    add def sorts_date and get_data

--- kursovaya-obr/utila.py
+++ kursovaya-obr/utila.py
@@ -1,4 +1,7 @@
 import json
+from datetime import datetime
+
+
 def loading_json(file_json):
     """
     Функция загружает данные из файла operations.json
@@ -11,10 +14,30 @@ def loading_json(file_json):
 
 def get_list_sorted(file):
     """
-    функция которая находит в файле operations.json пустой словарь и отсортировывает статус 'EXECUTED'
+    Функция которая находит в файле operations.json пустой словарь и отсортировывает статус 'EXECUTED'
     :param file: подается файл который получается в результате функции loading_json(file_json)
     :return: список словарей со статусами операций 'EXECUTED'
     """
-    json_selection = list(filter(lambda a: len(a) and a['state'] == 'EXECUTED', file))
+    json_selection = list(filter(lambda x: len(x) and x['state'] == 'EXECUTED', file))
     return json_selection
+
+
+def sorts_date(json_selection):
+    """
+    Функция которая сортирует списко словарей json_selection по дате на убывание (с верху самая крайняя дата)
+    :param json_selection: список полученный в результате вызова функции get_list_sorted(file)
+    :return: отсортерованный список по дате (Сверху списка находятся самые последние операции (по дате))
+    """
+    json_sort = sorted(json_selection,key=lambda x: datetime.strptime(x["date"], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
+    return json_sort
+
+
+def get_date(date):
+    """
+    Функция которая выдает дату операции
+    :param date: дада из файла operratuons.json
+    :return: дату в формате 14.10.2018
+    """
+    return f"{date[8:10]}.{date[5:7]}.{date[:4]}"
+
 
