84639ca1ddd5e5d45b2ebeb2108b8a1f6cb7af62
RViktorV <rgavih@yandex.ru>
2024-04-03 15:04:07 +0700

    test_get_converted_amount

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import os
 from config import ROOT_DIR
-from kursovaya_obr.utils import loading_json, get_list_sorted, sorts_date
+from kursovaya_obr.utils import loading_json, get_list_sorted, sorts_date, get_date, get_num_carte, get_converted_amount
 
 def test_loading_json():
     TEST_PATH = os.path.join(ROOT_DIR, 'tests', 'test.json')
@@ -63,3 +63,28 @@ def test_sorts_date():
     }
 ]
 
+
+def test_get_date():
+    assert get_date("2019-07-03T18:35:29.512364") == '03.07.2019'
+
+
+def test_get_num_carte():
+    assert get_num_carte("Maestro 1596837868705199") == 'Maestro 83 •• •••• 5199'
+    assert get_num_carte("Счет 64686473678894779589") == 'Счет **9589'
+
+
+def test_get_converted_amount():
+    assert get_converted_amount({
+    "id": 587085106,
+    "state": "EXECUTED",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  }) == '48223.05 руб.'
